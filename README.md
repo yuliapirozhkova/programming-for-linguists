07da9b6ce0db40d6717b4ea5c659884829ddd1d3
Artyom Tugaryov <artyom.tugarev@intel.com>
2021-01-27 17:15:17 +0300

    Add initial stack

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Programming for linguists
+
+This repository contains materials for a programming course aimed at students of 2–3 years of study, not in software development areas.
+Students are expected to have minimal experience in programming.
+
+This course covers many different topics. All topics are split into N groups: Data structure
+
+##Data Structure
+1. [Stack](data_structures/stack/README.md) is one of the simplest, and most basic data structures in computer science.
+
---
+++ data_structures/stack/stack.py
@@ -0,0 +1,46 @@
+"""
+Programming for linguists
+
+Implementation of the data structure "Stack"
+"""
+
+from typing import Iterable
+
+
+class Stack:
+    """
+    Stack Data Structure
+    """
+
+    def __init__(self, data: Iterable = None):
+        pass
+
+    def push(self, element):
+        """
+        Add the element ‘element’ at the top of stack
+        :param element: element to add to stack
+        """
+
+    def pop(self):
+        """
+        Delete the element on the top of stack
+        """
+
+    def top(self):
+        """
+        Return the element on the top of stack
+        :return: the element that is on the top of stack
+        """
+
+    def size(self) -> int:
+        """
+        Return the number of elements in stack
+        :return: Number of elements in stack
+        """
+
+    def empty(self) -> bool:
+        """
+        Return whether stack is empty or not
+        :return: True if stack does not contain any elements
+                 False if stack contains elements
+        """
